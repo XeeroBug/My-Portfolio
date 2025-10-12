4805821d2aaa5b8e6d6432f2f88781e539cb7e8e
XeeroBug <xeerobug@gmail.com>
2025-10-12 12:27:22 +0100

    Image added

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
 import { Card } from "@/components/ui/card";
 import { Testimonial } from "./mentors";
+import MyPic from "@/assets/images/animated-pic.png";
+import { BackgroundGradient } from "./ui/background-gradient";
 
 export function AboutMe() {
   return (
@@ -11,18 +13,26 @@ export function AboutMe() {
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 0.6 }}
-          className="text-center mb-16"
+          className="text-center mb-16 "
         >
           <h2 className="text-4xl md:text-5xl font-bold mb-4">About Me</h2>
-          <p className="text-muted-foreground text-lg">
-            Hi, I’m Usama Abdulhakeem, a passionate Full Stack Developer who
-            loves building secure, scalable, and user-friendly applications. I
-            work with React, NextJs, NestJS, MongoDB and others to create clean,
-            efficient solutions. I’ve had the privilege of serving at different
-            organizations, where my work and technical contributions were highly
-            appreciated. I’m a strong communicator, quick learner, and always
-            excited to take on challenges that drive real impact.
-          </p>
+
+          <BackgroundGradient className="rounded-3xl grid grid-cols-3 p-4 sm:p-10 bg-secondary">
+            <img
+              src={MyPic.src}
+              alt="my-pic"
+              className="object-contain size-96  rounded-2xl"
+            />
+            <p className="text-muted-foreground text-2xl col-span-2">
+              Hi, I’m Usama Abdulhakeem, a passionate Full Stack Developer who
+              loves building secure, scalable, and user-friendly applications. I
+              work with React, NextJs, NestJS, MongoDB and others to create
+              clean, efficient solutions. I’ve had the privilege of serving at
+              different organizations, where my work and technical contributions
+              were highly appreciated. I’m a strong communicator, quick learner,
+              and always excited to take on challenges that drive real impact.
+            </p>
+          </BackgroundGradient>
         </motion.div>
 
         <motion.div
@@ -30,7 +40,9 @@ export function AboutMe() {
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
         >
-          <h2 className="text-3xl md:text-4xl font-bold mb-6 text-center">My Mentors</h2>
+          <h2 className="text-3xl md:text-4xl font-bold mb-6 text-center">
+            My Mentors
+          </h2>
 
           <Card className="overflow-hidden h-full flex flex-col bg-card/50 backdrop-blur-sm border-border/50 hover:border-primary/50 transition-all group">
             <Testimonial />
